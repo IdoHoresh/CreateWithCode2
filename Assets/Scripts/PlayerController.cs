d8948275ad172a9332291c613a8380c66d8da76f
Ido Horesh <ihoresh07@gmail.com>
2025-01-03 13:49:38 +0200

    Added box collider for the player and made the player jump at start

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody rigidbody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(Vector3.up * 1000);
     }
 
     // Update is called once per frame
